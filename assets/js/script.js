cb6892272ca4d0d6a9898822d899223a1b358c93
Lauren Groh <legroh@uwm.edu>
2021-10-03 16:57:15 -0500

    add date to to 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,28 @@ var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
 var uvIndex = document.querySelector("#uv-index");
 var date = document.querySelector("#date");
+var date1 = document.querySelector("#date1"); 
+var date2 = document.querySelector("#date2");
+var date3 = document.querySelector("#date3");
+var date4 = document.querySelector("#date4");
+var date5 = document.querySelector("#date5");
 // var day2 = document.querySelector("#day2");
 // var day3 = document.querySelector("#day3");
 // var day4 = document.querySelector("#day4");
 // var day5 = document.querySelector("#day5");
 
+
 // Display date
-currentDate = moment().format('MM/DD/YYYY');
+var currentDate = moment().format('MM/DD/YYYY');
 date.append(currentDate);
+
+var displayDate1 = moment().add(1,'days').format('MM/DD/YYYY');
+var displayDate2 = moment().add(2,'days').format('MM/DD/YYYY');
+var displayDate3 = moment().add(3,'days').format('MM/DD/YYYY');
+var displayDate4 = moment().add(4,'days').format('MM/DD/YYYY');
+var displayDate5 = moment().add(5,'days').format('MM/DD/YYYY');
+
+
 
 searchBtn.addEventListener('click', function(){
     var citySearch = document.querySelector("#city-search").value;
@@ -50,6 +64,11 @@ searchBtn.addEventListener('click', function(){
         })
         .then(function(data) {
             console.log(data);
+            date1.append(displayDate1);
+            date2.append(displayDate2);
+            date3.append(displayDate3);
+            date4.append(displayDate4);
+            date5.append(displayDate5);
             fiveDayPopulate(1, data.list[6]);
             fiveDayPopulate(2, data.list[14]);
             fiveDayPopulate(3, data.list[22]);
